2bec831ff6baa0cf75a13f10be9129c23d11f22f
rowlettron <rowlettron@yahoo.com>
2022-10-02 12:39:53 -0500

    File changes
    Added .png to the .gitignore
    Added addition of chart of hurricane strength by winds.

--- Hurricanes.py
+++ Hurricanes.py
@@ -31,6 +31,50 @@ def categorize(row):
     else:
         return 1
 
+def number_of_hurricanes(df):
+    df_by_year = df.groupby(["ReportedYear"], as_index=False)["HurricaneFlag"].sum()
+    # df_by_year = df_hurricane.groupby("ReportedYear").agg({'HurricaneFlag': ['sum']})
+    # # print(type(df_by_year))
+    # print(df_by_year)
+
+    df_by_year.rename(columns = {'ReportedYear': 'Year',
+                                 'HurricaneFlag':'TotalHurricanes'}, inplace=True)
+
+    # print(df_by_year)
+
+    df_by_year.plot(x="Year", 
+                    y="TotalHurricanes", 
+                    marker=".", 
+                    kind="line")
+
+    plt.title("Number of Hurricanes by Year That Made Landfall\n1851 - 2021")
+    plt.xlabel("Year Reported")
+    plt.ylabel("Number of Hurricanes Making Landfall")
+
+    plt.show()
+
+def strength_of_hurricanes(df):
+    df_by_year = df.groupby(["ReportedYear"], as_index=False)["MaxWind_mph"].max()
+    # df_by_year = df_hurricane.groupby("ReportedYear").agg({'MaxWind_mph': ['sum']})
+    # # print(type(df_by_year))
+    # print(df_by_year)
+
+    df_by_year.rename(columns = {'ReportedYear': 'Year',
+                                 'MaxWind_mph':'MaxStrength'}, inplace=True)
+
+    # print(df_by_year)
+
+    df_by_year.plot(x="Year", 
+                    y="MaxStrength", 
+                    marker=".", 
+                    kind="line")
+
+    plt.title("Maximum Strength of Hurricanes (mph) by Year That Made Landfall\n1851 - 2021")
+    plt.xlabel("Year Reported")
+    plt.ylabel("Maximum Strength of Hurricanes (mph) Making Landfall")
+
+    plt.show()
+
 def main():
 
     currentDirectory = os.getcwd()
@@ -49,27 +93,12 @@ def main():
     df_hurricane = load_file_to_df(excelFile)
 
     df_hurricane['HurricaneFlag'] = df_hurricane.apply(lambda row: categorize(row), axis=1)
-    print(df_hurricane.head(30))
 
-    # df_by_year = df_hurricane.groupby(["ReportedYear"], as_index=False)["Name"].count()
-    # # print(type(df_by_year))
-    # # print(df_by_year)
+    number_of_hurricanes(df_hurricane)
 
-    # df_by_year.rename(columns = {'ReportedYear': 'Year',
-    #                              'Name':'NumberOfHurricanes'}, inplace=True)
+    strength_of_hurricanes(df_hurricane)
 
-    # print(df_by_year)
 
-    # df_by_year.plot(x="Year", 
-    #                 y="NumberOfHurricanes", 
-    #                 marker=".", 
-    #                 kind="line")
-
-    # plt.title("Number of Hurricanes by Year That Made Landfall\n1851 - 2021")
-    # plt.xlabel("Year Reported")
-    # plt.ylabel("Number of Hurricanes Making Landfall")
-
-    # plt.show()
 
 ################# Main Processing Section ##############################
 
